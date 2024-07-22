dc8286dff4cb79eeb1dcc9e934dfeae15cb7cb99
Карпенко Алексей <karpenko@echo-company.ru>
2024-07-22 22:47:34 +0300

    feat: функционал нарезки refactor: подчистка лишний файлов

--- video-split.py
+++ video-split.py
@@ -1,15 +1,11 @@
-#!/usr/bin/env python
-
 from __future__ import print_function
 
-import csv
-import json
 import math
 import os
 import shlex
 import subprocess
-from optparse import OptionParser
-from os import listdir
+
+RATIO = 0.85
 
 
 def get_video_length(filename):
@@ -25,7 +21,7 @@ def ceildiv(a, b):
     return int(math.ceil(a / float(b)))
 
 
-def split_by_seconds(filename, split_length, vcodec="copy", acodec="copy",
+def split_by_seconds(filename, split_length, dir_name, vcodec="copy", acodec="copy",
                      extra="", video_length=None, **kwargs):
     if split_length and split_length <= 0:
         print("Split length can't be 0")
@@ -33,6 +29,7 @@ def split_by_seconds(filename, split_length, vcodec="copy", acodec="copy",
 
     if not video_length:
         video_length = get_video_length(filename)
+
     split_count = ceildiv(video_length, split_length)
     if split_count == 1:
         print("Video length is less then the target split length.")
@@ -42,8 +39,6 @@ def split_by_seconds(filename, split_length, vcodec="copy", acodec="copy",
     try:
         filebase = ".".join(filename.split(".")[:-1])
         fileext = filename.split(".")[-1]
-
-        print(filebase, fileext)
     except IndexError as e:
         raise IndexError("No . in filename. Error: " + str(e))
     for n in range(0, split_count):
@@ -54,22 +49,29 @@ def split_by_seconds(filename, split_length, vcodec="copy", acodec="copy",
             split_start = split_length * n
 
         split_args += ["-ss", str(split_start), "-t", str(split_length),
-                       filebase + "-" + str(n + 1) + "-of-" +
+                       dir_name + "/" + filebase + "-" + str(n + 1) + "-of-" +
                        str(split_count) + "." + fileext]
+
         print("About to run: " + " ".join(split_cmd + split_args))
-        # subprocess.check_output(split_cmd + split_args)
+        subprocess.check_output(split_cmd + split_args)
 
 
 def main():
-    videos = [f for f in listdir('.') if f.endswith('.mp4')]
+    videos = [f for f in os.listdir('.') if os.path.isfile(f) and f.endswith('.mp4')]
     for video in videos:
+
+        dir_name = "splits-" + video.replace(' ', '-')
+        if os.path.isdir(dir_name):
+            continue
+
+        os.mkdir(dir_name)
 
         video_length = get_video_length(video)
         file_size = os.stat(video).st_size
-        split_filesize = int(1024*1024*20 * 0.85)
+        split_filesize = int(1024 * 1024 * 20 * RATIO)
 
         split_length = int(split_filesize / float(file_size) * video_length)
-        split_by_seconds(video, split_length, video_length=video_length)
+        split_by_seconds(video, split_length, dir_name, video_length=video_length)
 
 
 if __name__ == '__main__':
